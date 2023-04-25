27a389b73a144afa42949adc4602c8c225bdb1f9
reishaladwa <86329309+reishaladwa@users.noreply.github.com>
2023-04-25 00:26:43 -0700

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "odysseywebappafd4e4ea": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello World test
+//     </div>
+//   );
+// }
+
+// export default App;
+// import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
   return (
-    <div className="App">
-      Hello World test
-    </div>
+    <View className="App">
+      <Card>
+        {/* <Image src={logo} className="App-logo" alt="logo" /> */}
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
