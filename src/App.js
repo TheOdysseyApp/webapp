b98d458b65c3643a914c011a027ad6e2730ef83f
kevin <kevidn15@uci.edu>
2023-05-03 16:21:53 -0700

    Testing redeploy

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App({ signOut }) {
     <View className="App">
       <Card>
         {/* <Image src={logo} className="App-logo" alt="logo" /> */}
-        <Heading level={1}>We now have Auth!</Heading>
+        <Heading level={1}>We now have Auth AND we can redeploy!</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
