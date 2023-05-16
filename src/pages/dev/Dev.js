6d7c570fd3285e2f67331b4c5c5ccda887523115
ryan-lacasse <rlacasse@uci.edu>
2023-05-16 09:54:50 -0700

    datetime picker

--- src/pages/dev/Dev.js
+++ src/pages/dev/Dev.js
@@ -1,17 +1,23 @@
+import React, { useState } from 'react';
 import { TextField } from "@aws-amplify/ui-react";
+import DateTime from '../../components/datetime/DateTime';
+
 
 function Dev() {
+    const [text, setText] = useState(null);
+    const [date, setDate] = useState(null);
 
     return (
         <div className="dev">
             <div className="container">
-                <h3>hi</h3>
+                <h3>dev workbench</h3>
                 <TextField
                     placeholder="..."
-                    label="don't change this input value"
-                    value="it will break prod"
-                    onChange={(e) => this.iWarnedYou}
+                    label="field label"
+                    defaultValue='default value'
+                    onChange={(e) => setText(e.target.value)}
                 ></TextField>
+                <DateTime onChange={setDate} value={date} label='datetime test'/>
             </div>
         </div>
     )
