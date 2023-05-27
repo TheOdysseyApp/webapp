f43eb8ca66dd9689ad3a32e226647de47665003f
Lina Pav <lpav@uci.edu>
2023-05-26 23:47:24 -0700

    update confirmation page

--- src/components/confirmation/Confirmation.js
+++ src/components/confirmation/Confirmation.js
@@ -4,9 +4,10 @@ function Confirmation() {
     return (
         <div className='confirmation'>
             <div className='container'>
-
-                Your code goes here!
-
+              <h1 className = "Success">Success!</h1>
+              <p className="Traveler">Itinerary has been sent to Liam Johnson</p>
+              <p className="IDnum">0b24c9ac-1b6c-4a85-8b2d-2d40e208b87c</p>
+              <p className="Days"># Days in Location</p>
             </div>
         </div>
     )
