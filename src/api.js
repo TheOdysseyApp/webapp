7ab28c8bde342febf01a7e48d866732515709951
ryan-lacasse <rlacasse@uci.edu>
2023-05-29 20:14:11 -0700

    post request (missing endpoint)

--- src/api.js
+++ src/api.js
@@ -32,7 +32,27 @@ export const fetchItineraries = async (currentTripId) => {
             return {}
         }
         return unmarshall(json.Item)
-      } catch (error) {
+    } catch (error) {
         console.error(error);
-      }
+    }
+}
+
+export const createItinerary = async(tripDetails) => {
+    try {
+        const response = await fetch(
+            "URL GOES HERE",
+            {headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(tripDetails)
+        });
+        if (!response.ok) {
+            throw new Error('Error submitting data');
+        }
+        return response;
+    } catch (error) {
+        console.error(error);
+    }
 }
