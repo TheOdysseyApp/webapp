62c9453229d4cf072025387fb6c24f4c4a085004
Kevin Nguyen <kevidn15@uci.edu>
2023-06-01 12:18:49 -0700

    removed temporary link

--- src/components/trip-details/TripDetails.js
+++ src/components/trip-details/TripDetails.js
@@ -139,10 +139,9 @@ function TripDetails({ traveler, destination, setCurrentTrip, forwardStage, back
                             label="Room Type*"
                             defaultValue={item.roomType}
                             onChange={(e) => item.roomType = e.target.value} />
-                        {/* Image is hardcoded for the demo, change later */}
                         <TextField
                             label="Hotel Image*"
-                            defaultValue={"https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80"}
+                            defaultValue={""}
                             onChange={(e) => item.stayImgURL = e.target.value} />
                         <Divider />
                     </div>
