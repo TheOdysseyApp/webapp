42639a8a80ee9ca7524edd9108f8ec035c96ed17
Lina Pav <lpav@uci.edu>
2023-06-06 14:15:17 -0700

    resolved merge conflict

--- src/components/traverler-info/TravelerInfo.js
+++ src/components/traverler-info/TravelerInfo.js
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-import { Card, View, Grid } from "@aws-amplify/ui-react";
-import CompareDestination from "../compare-destination/CompareDestination"
-import './TravelerInfo.css'
-import React, { useState } from 'react';
-
-function TravelerInfo({ currentTripId, traveler, trip, setCurrentDestination, forwardStage }) {
-  console.log(trip)
-
-  // Convert trip.activities object to a string
-  const activitiesString = trip && trip.activities ? Object.values(trip.activities).join(', ') : '';
-
-  return (
-    <Card
-      columnStart="2"
-      columnEnd="-1"
-      className="TravelerInfo"
-    >
-      {
-        (currentTripId && traveler && trip && Object.keys(trip).length !== 0) ? 
-        <View className="TravelerInfoSelected">
-          <div className="header">
-            <h1>{traveler.first_name + " " + traveler.last_name}</h1>
-            <p>{`# ${traveler.tripId}`}</p>
-            <div>
-              <p>{`Departing from: `}{trip.departingFrom}</p>
-              <p>{`Month of trip: `}{trip.month}</p>
-              <p>{`Number of Days: `}{trip.duration}</p>
-            </div>
-            <p>{`Desired Activities: `}{activitiesString}</p>
-            <p>{`Budget: `}${trip.minBudget}-${trip.maxBudget}</p>
-          </div>
-          <Grid
-            templateColumns="1fr 1fr 1fr"
-            className="options"
-          >
-            <CompareDestination destination={trip.A} column={1} setCurrentDestination={setCurrentDestination} forwardStage={forwardStage} />
-            <CompareDestination destination={trip.B} column={2} setCurrentDestination={setCurrentDestination} forwardStage={forwardStage} />
-            <CompareDestination destination={trip.C} column={3} setCurrentDestination={setCurrentDestination} forwardStage={forwardStage} style={{border: "none"}}/>
-          </Grid>
-        </View>
-        : (trip && Object.keys(trip).length === 0) ? 
-        <View className="TravelerInfoUnselected">
-          <h3 className="noTraveler">Server error</h3>
-        </View>
-        :
-        <View className="TravelerInfoUnselected">
-          <h3 className="noTraveler">Select a traveler to get started</h3>
-        </View>
-      }
-    </Card>
-  )
-}
-
-export default TravelerInfo;
-
-=======
 import { Card, View, Grid } from "@aws-amplify/ui-react";
 import CompareDestination from "../compare-destination/CompareDestination"
 import './TravelerInfo.css'
@@ -111,4 +54,3 @@ function TravelerInfo({ currentTripId, traveler, trip, setCurrentDestination, co
 }
 
 export default TravelerInfo;
->>>>>>> 5d0222eb647870d06c6cd4bdfc4e4858f8642ff2
