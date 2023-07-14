759fe006dca1cd7b9a8e2edbee7dbe58e4b423d2
smash-96 <maisamshah57@gmail.com>
2023-07-14 13:25:39 +0500

    miss

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { ItinerariesList } from './itinerarieslist/ItinerariesList'
-export { CompareTrips } from './stages/compareTrips/CompareTrips'
-export { Confirmation } from './stages/confirmation/Confirmation'
-export { EditTrip } from './stages/editTrip/EditTrip'
-export { Review } from './stages/reviewTrip/ReviewTrip'
+export { ItinerariesList } from './itinerarieslist/ItinerariesList';
+export { CompareTrips } from './stages/compareTrips/CompareTrips';
+export { Confirmation } from './stages/confirmation/Confirmation';
+export { EditTrip } from './stages/editTrip/EditTrip';
+export { Review } from './stages/reviewTrip/ReviewTrip';
