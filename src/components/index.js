473ca12bdb124383ca8ef1ebc145ba6ae01118ab
smash-96 <maisamshah57@gmail.com>
2023-07-14 15:17:52 +0500

    miss v2

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { ItinerariesList } from '../../src/components/itinerarieslist/ItinerariesList';
 export { CompareTrips } from './stages/compareTrips/CompareTrips';
 export { Confirmation } from './stages/confirmation/Confirmation';
 export { EditTrip } from './stages/editTrip/EditTrip';
 export { Review } from './stages/reviewTrip/ReviewTrip';
+export { ItinerariesList } from '../../src/components/itinerarieslist/ItinerariesList';
