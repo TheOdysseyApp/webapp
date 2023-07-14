2455e9856019ea114ac82eb5325f01f95fd5e1ba
smash-96 <maisamshah57@gmail.com>
2023-07-14 15:02:06 +0500

    try

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { ItinerariesList } from './itinerarieslist/ItinerariesList';
+export { ItinerariesList } from '../components/itinerarieslist/ItinerariesList';
 export { CompareTrips } from './stages/compareTrips/CompareTrips';
 export { Confirmation } from './stages/confirmation/Confirmation';
 export { EditTrip } from './stages/editTrip/EditTrip';
